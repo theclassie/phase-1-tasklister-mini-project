894e4d4f36f1ef367792fe5b7d61bbd69d31c9b0
tristyn <trisclassie@gmaiil.com>
2022-08-18 20:18:12 -0600

    my tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log('Loaded')
+    const taskListForm = document.getElementById("create-task-form")
+    taskListForm.addEventListener("submit", addTask)
 });
+
+const addTask = (event) => {
+  event.preventDefault()
+  const newTask = event.target.querySelector('#new-task-description').value
+  const pTag = document.createElement('li')
+  pTag.textContent = `${newTask} `
+  // console.log(pTag)
+  const task = document.getElementById('list')
+  // console.log(task)
+  task.appendChild(pTag)
+  event.target.querySelector('#new-task-description').value = ""
+  let btn = document.createElement('button')
+  btn.addEventListener('click', removeTask)
+  btn.textContent = '  x'
+  pTag.appendChild(btn)
+  // console.log(pTag)
+}
+
+
+function removeTask(event){
+  event.target.parentNode.remove()
+} 
